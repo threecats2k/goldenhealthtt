69fddb9d5d6ef836f7d1082e38b57a1330aef20e
doanthai <doanquocthai.it@gmail.com>
2025-12-28 16:16:02 +0700

    feat: update phone

--- update_i18n_headers.js
+++ update_i18n_headers.js
@@ -13,7 +13,7 @@ const htmlFiles = [
 
 // Common header pattern with i18n
 const headerTemplate = `    <link rel="stylesheet" href="css/styles.css">
-    <link rel="canonical" href="https://caugiayhotel.store/">
+    <link rel="canonical" href="https://goldenthientam.store/">
     <script src="js/i18n.js" defer></script>
 </head>
 <body>
